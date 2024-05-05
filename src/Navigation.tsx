93e70b3de50a1bcf7c33bb189d9f90db7fa913de
Deepak Sisodiya <deepaksisodiya@Deepaks-MacBook-Pro.local>
2024-05-05 19:27:08 +0530

    improve file structure

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -2,10 +2,16 @@ import { Link } from 'react-router-dom';
 
 export default function Navigation() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+    <div className="navbar">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+      <Link className="nav-link" to="/about">
+        About
+      </Link>
+      <Link className="nav-link" to="/contact">
+        Contact
+      </Link>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import router from './routes.tsx';
+import router from './routes/Routes.tsx';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 

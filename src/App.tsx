960cdd8d36f390bfa380674139aacc57a3da8729
Deepak Sisodiya <deepak.sisodiya@technogise.com>
2024-05-07 18:04:05 +0530

    added material ui library

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,13 @@ import { Outlet } from 'react-router-dom';
 import Logo from './assets/react.svg';
 import Todos from './modules/todos/Todos';
 
+import Button from '@mui/material/Button';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
   return (
     <>
@@ -10,6 +17,7 @@ function App() {
       <h2>API End Point: {JSON.stringify(import.meta.env.VITE_APP_API_URL)}</h2>
       <Logo />
       <h1>React Template</h1>
+      <Button variant="contained">Hello world</Button>
       <div id="detail">
         <Outlet />
       </div>

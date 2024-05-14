3fe186a227f349c3b22b7fc713e78180adaadc55
Deepak Sisodiya <deepak.sisodiya@technogise.com>
2024-05-14 10:41:59 +0530

    added svg supporr

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Navigation from './Navigation';
 import { Outlet } from 'react-router-dom';
+import Logo from './assets/react.svg';
 
 function App() {
   return (
     <>
       <Navigation />
       <h2>API End Point: {JSON.stringify(import.meta.env.VITE_APP_API_URL)}</h2>
+      <Logo />
       <h1>React Template</h1>
       <div id="detail">
         <Outlet />
